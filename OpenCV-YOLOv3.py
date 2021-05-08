63018c15156e8fd3acbbc76e313590fd28552675
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-05-08 23:30:25 +0900

    Create OpenCV-YOLOv3.py

---
+++ OpenCV-YOLOv3.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+img = cv2.imread("clean_d3.png")     # input image
+height, width, channels = img.shape
+
+blob = cv2.dnn.blobFromImage(img, 1/255.0, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.3:
+            # Object detected
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+            # 좌표
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+            
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+font = cv2.FONT_HERSHEY_PLAIN
+
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[i]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        text="{}: {:.4f}".format(label,confidences[i])
+        print(text)
+        cv2.putText(img, text, (x, y -5), font, 0.8, color, 1)
+        
+cv2.imwrite('yolo_result/clean_d3.png', img)
